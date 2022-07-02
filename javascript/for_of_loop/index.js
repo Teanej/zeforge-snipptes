7de58f65929d90840fd324374cd38818ff0c1384
Amar Cerim <amarcerim8@gmail.com>
2022-07-02 14:59:34 +0200

    added js for of loop snippet

---
+++ javascript/for_of_loop/index.js
@@ -0,0 +1,14 @@
+const zeforge = [
+    'zenica ',
+    'developers',
+    'community'
+  ];
+  for (let naziv of sviFajlovi) {
+    console.log(naziv);
+  }
+  
+ /****************ZEFORGE****************** 
+ for of petljom u JavaScript-u 
+ prolazimo kroz sve elemente niza zeforge 
+ i ispisujemo ih u konzolu browsera 
+*****************************************/  
